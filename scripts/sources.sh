8abf549db21095398489fc19556fbf32d49c60b0
Mikhail Hyde <hyde.mikhail@gmail.com>
2024-04-17 21:12:36 -0400

    zoxide for personal

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -8,3 +8,14 @@ else
     git clone git@github.com:neovim/neovim.git $HOME/Documents/sources/neovim
 fi
 
+# Zoxide
+if [ -d "$HOME/Documents/sources/zoxide" ]; then
+    echo "Zoxide source already on disk"
+else
+    echo "Cloning zoxide repository"
+    git clone git@github.com:ajeetdsouza/zoxide.git $HOME/Documents/sources/zoxide
+    CURR_DIR=$(pwd)
+    cd $HOME/Documents/sources/zoxide
+    ./install.sh
+    cd $CURR_DIR
+fi
